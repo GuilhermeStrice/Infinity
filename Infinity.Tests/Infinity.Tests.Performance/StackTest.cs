f9551586a9846cee9a345873def991aabc610147
GuilhermeStrice <15857393+GuilhermeStrice@users.noreply.github.com>
2024-01-09 19:36:17 +0100

    Stack and ConcurrentStack performance test

---
+++ Infinity.Tests/Infinity.Tests.Performance/StackTest.cs
@@ -0,0 +1,64 @@
+using Infinity.Core;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using Xunit.Abstractions;
+
+namespace Infinity.Tests.Performance
+{
+    public class StackTest
+    {
+        ITestOutputHelper output;
+
+        public StackTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        Stack<int> first = new Stack<int>();
+        readonly object first_lock = new object();
+        ConcurrentStack<int> second = new ConcurrentStack<int>();
+        Stopwatch sw1 = new Stopwatch();
+        Stopwatch sw2 = new Stopwatch();
+
+        [Fact]
+        public void Locking_performance_test()
+        {
+            int val = 0;
+
+            sw1.Start();
+
+            Parallel.For(1, 10000, (n) =>
+            {
+                lock (first)
+                {
+                    first.Push(n);
+                    if (n == 9999)
+                        val = first.Pop();
+                }
+            });
+
+            sw1.Stop();
+
+            output.WriteLine($"{val}");
+
+            sw2.Start();
+
+            Parallel.For(1, 10000, (n) =>
+            {
+                second.Push(n);
+                if (n == 9999)
+                    second.TryPop(out int val);
+            });
+
+            sw2.Stop();
+
+            output.WriteLine($"{val}");
+            
+            output.WriteLine("");
+
+            output.WriteLine(sw1.ElapsedTicks.ToString());
+            output.WriteLine(sw2.ElapsedTicks.ToString());
+            // ConcurrentStack is faster
+        }
+    }
+}
